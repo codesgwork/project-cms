31b1140ab9f4d201aead53a026af9ccb443a78d1
inusky <inuskydotcom@gmail.com>
2025-09-16 12:18:51 +0530

    Update ./config/database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,11 +1,13 @@
 export default ({ env }) => {
-  console.log('DEBUG: DATABASE_CLIENT =', env('DATABASE_CLIENT'));
-  console.log('DEBUG: DATABASE_URL =', env('DATABASE_URL'));
+  const ssl = env.bool('DATABASE_SSL', false);
 
   return {
     connection: {
       client: env('DATABASE_CLIENT', 'postgres'),
-      connection: env('DATABASE_URL'),
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        ssl: ssl ? { rejectUnauthorized: false } : false,
+      },
       pool: { min: 0, max: 10 },
       acquireConnectionTimeout: 60000,
     },
